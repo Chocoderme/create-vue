f86e0c98b99db9e96d20f32de0ab0ebe48627c5a
Elliott Zwertvaegher <elliott.zz59@gmail.com>
2022-04-16 15:32:32 +0900

    chore: Upgrade version + change submodule

--- scripts/prepublish.mjs
+++ scripts/prepublish.mjs
@@ -9,7 +9,7 @@ let { version } = JSON.parse(await fs.readFile('./package.json'))
 const playgroundDir = path.resolve(__dirname, '../playground/')
 cd(playgroundDir)
 
-await $`git add -A .`
+await $`git add -A`
 try {
   await $`git commit -m "version ${version} snapshot"`
 } catch (e) {
